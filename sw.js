12e611954728ccb1012af86d3e4d31832f9003ab
toast <micahpcal@gmail.com>
2025-11-19 14:16:55 -0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('mcbe-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',         // homepage
+        '/index.html',
+        '/manifest.json',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png'
+      ])
+    })
+  )
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  )
+})
